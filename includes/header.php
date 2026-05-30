9ed02ddfaf379cbdaf1502e7f608974b74a03cfa
Rapture Admin <admin@rapturetherapycentre.com>
2026-05-30 22:51:59 +0530

    Format mobile promo banner to column layout with no forced line breaks in text

--- includes/header.php
+++ includes/header.php
@@ -17,7 +17,7 @@ $base = isset($is_article) ? '../' : '';
     <meta name="description" content="<?= htmlspecialchars($page_description) ?>">
     <title><?= htmlspecialchars($page_title) ?></title>
     <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?= $base ?>css/style.css?v=10.0">
+    <link rel="stylesheet" href="<?= $base ?>css/style.css?v=11.0">
     <link rel="icon" type="image/png" href="<?= $base ?>assets/logo.png">
     <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -32,7 +32,7 @@ $base = isset($is_article) ? '../' : '';
 
     <!-- Top Promo Banner -->
     <div class="insurance-promo-banner">
-        <span>Schedule a 1-hour consultation<br class="mobile-break"> with our lead therapist!</span>
+        <span>Schedule a 1-hour consultation with our lead therapist!</span>
         <a href="#booking-modal" class="open-booking-modal">Book Now <i class="ri-arrow-right-line"></i></a>
     </div>
 
